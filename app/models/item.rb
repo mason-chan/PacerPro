e34dcb63c2fcc5d9f261a637dd1336510f7b6510
mctekno <92968560+mason-chan@users.noreply.github.com>
2023-12-08 04:29:55 -0500

    Add comments

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,15 @@
 class Item < ApplicationRecord
     validates :name, presence: true
 
-    # Soft delete 
+    # Default scope to exclude "deleted" items from normal queries
     default_scope -> { where(deleted_at: nil) }
 
+    # Add method "soft_delete" that updates the "deleted_at" attribute with the current timestamp
     def soft_delete
         update(deleted_at: Time.now)
     end
 
+    # Add method "restore" that sets the "deleted_at" attribute to nil
     def restore
         update(deleted_at: nil)
     end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,21 +4,25 @@ RSpec.describe Item, type: :model do
   describe 'soft delete functionality' do
     let(:item) { Item.create(name: 'Test Item') }
 
+    # Test item is instantiated with nil
     it 'item instantiated with nil deleted_at' do
       expect(item.deleted_at).to be_nil
     end
 
+    # Test soft deleting an item
     it 'soft deletes an item' do
       item.soft_delete
       expect(item.deleted_at).to be_present
     end
 
+    # Test restoring a soft-deleted item
     it 'restores a soft-deleted item' do
-      #item.soft_delete
+      item.soft_delete
       item.restore
       expect(item.deleted_at).to be_nil
     end
 
+    # Test that soft-deleted items are excluded from normal queries
     it 'excludes soft-deleted items from normal queries' do
       soft_deleted_item = Item.create(name: 'Soft Deleted Item')
       soft_deleted_item.soft_delete
